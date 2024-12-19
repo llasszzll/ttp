faf1b353eb2c875d2fa14e91c5f1a40e429128ce
llasszzll@gmail.com <llasszzll@gmail.com>
2024-12-20 01:30:42 +0200

    Adding Message

--- frontend/src/features/tickets/ticketSlice.js
+++ frontend/src/features/tickets/ticketSlice.js
@@ -49,6 +49,18 @@ export const getTicket = createAsyncThunk('tickets/get', async (ticketId, thunkA
     }
 })
 
+// CLOSE User ticket
+export const closeTicket = createAsyncThunk('tickets/close', async (ticketId, thunkAPI) => {
+    try {
+        const token = thunkAPI.getState().auth.user.token
+        return await ticketService.closeTicket(ticketId, token)
+    } catch (error) {
+        const message = (error.response && error.response.data && error.response.data.message)
+            || error.message || error.toString()
+
+        return thunkAPI.rejectWithValue(message)
+    }
+})
 
 export const ticketSlice = createSlice({
     name: 'ticket',
@@ -108,7 +120,10 @@ export const ticketSlice = createSlice({
             state.isError = true
             state.message = action.payload
         })
-
+        builder.addCase(closeTicket.fulfilled, (state, action) => {
+            state.isLoading = false
+            state.tickets.map((ticket) => ticket._id === action.payload._id ? (ticket.status = 'closed') : ticket)
+        })
     }
 })
 
