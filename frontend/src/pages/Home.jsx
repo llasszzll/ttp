7e275ae7559eff2b7e92df9459873e3efcc225d4
llasszzll@gmail.com <llasszzll@gmail.com>
2024-12-19 14:12:20 +0200

    Frontend Auth fixed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
   return (
     <>
       <section className="heading">
-        <h1>How can we help you</h1>
+        <h1>How can we help you?</h1>
         <p>Please choose a option below</p>
       </section>
       <Link to="./new-ticket" className="btn btn-reverse btn-block">
